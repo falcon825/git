6322098724535bbcc58c17b90b86eb3979837b44
zhangjd <zhangjd825@outlook.com>
2021-05-24 22:44:14 +0800

    product second commit

--- src/main/java/com/code/Product.java
+++ src/main/java/com/code/Product.java
@@ -3,5 +3,7 @@ package com.code;
 public class Product {
     public static void main(String[] args) {
         System.out.println("product first commit");
+        //        product second commit
+        System.out.println("product second commit");
     }
 }
