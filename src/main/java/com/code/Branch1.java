9bd9657d4a9ea4076fa55d4b6769a59c1eb2868a
zhangjd <zhangjd825@outlook.com>
2021-05-24 22:23:47 +0800

    branch1 second commit

--- src/main/java/com/code/Branch1.java
+++ src/main/java/com/code/Branch1.java
@@ -3,5 +3,6 @@ package com.code;
 public class Branch1 {
     public static void main(String[] args) {
         System.out.println("hello branch1");
+        System.out.println("I modify something");
     }
 }
